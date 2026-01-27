e31766d371725f36d2224402dfe87bba384f40d3
Andrea Jama <142735173+AndreaJama@users.noreply.github.com>
2026-01-27 16:21:11 -0300

    Create create_table_nyc_taxi_trips_2024_01_stg.sql

---
+++ 02_integrar/database/ddl/create_table_nyc_taxi_trips_2024_01_stg.sql
@@ -0,0 +1,21 @@
+CREATE TABLE public.nyc_taxi_trips_2024_01 (
+    vendorid INTEGER,
+    tpep_pickup_datetime TIMESTAMP,
+    tpep_dropoff_datetime TIMESTAMP,
+    passenger_count INTEGER,
+    trip_distance NUMERIC,
+    ratecodeid INTEGER,
+    store_and_fwd_flag CHAR(1),
+    pulocationid INTEGER,
+    dolocationid INTEGER,
+    payment_type INTEGER,
+    fare_amount NUMERIC,
+    extra NUMERIC,
+    mta_tax NUMERIC,
+    tip_amount NUMERIC,
+    tolls_amount NUMERIC,
+    improvement_surcharge NUMERIC,
+    total_amount NUMERIC,
+    congestion_surcharge NUMERIC,
+    airport_fee NUMERIC
+);
